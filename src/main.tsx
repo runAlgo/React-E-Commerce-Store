0c90ee7db987946d9d0b56159907335f8bb6880f
runAlgo <150834861+runAlgo@users.noreply.github.com>
2024-11-27 20:31:43 +0545

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { FilterContextProvider } from "./components/FilterContext.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <FilterContextProvider>
+      <App />
+    </FilterContextProvider>
+  </StrictMode>
+);
